d53a84af6755f5cfea15552029c76ef964341af3
dpopchev <dpopchev@gmail.com>
2022-12-19 16:42:15 +0200

    extend telescope with grep live

--- src/config/nvim/after/plugin/telescope.lua
+++ src/config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,16 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pS', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+local actions = require('telescope.actions')
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = { ["<C-e>"] = actions.close, }
+		}
+	}
+})
