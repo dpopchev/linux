65e0a9b7137e7e14d2109c30e542696b7a8f506c
dpopchev <dpopchev@gmail.com>
2022-12-20 13:24:52 +0200

    add nvim surround

--- src/config/nvim/lua/dpopchev/packer.lua
+++ src/config/nvim/lua/dpopchev/packer.lua
@@ -69,5 +69,15 @@ return require('packer').startup(function(use)
         end
     }
 
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+
 
 end)
