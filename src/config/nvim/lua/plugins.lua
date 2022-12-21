1233b35a08316ae809311e3f13a4be61aa3a95d2
dpopchev <dpopchev@gmail.com>
2022-12-21 09:43:46 +0200

    add packer and sonokai theme

--- src/config/nvim/lua/plugins.lua
+++ src/config/nvim/lua/plugins.lua
@@ -1,1 +1,16 @@
-require('plugins')
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        "sainnhe/sonokai",
+        config = function()
+            vim.g.sonokai_style = 'maia'
+            vim.g.sonokai_better_performance = 1
+            vim.cmd "colorscheme sonokai"
+        end,
+    }
+end)
