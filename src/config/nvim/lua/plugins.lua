b444aaa58135c774b1ba2ec60d96a839edbebe70
dpopchev <dpopchev@gmail.com>
2022-12-23 15:52:43 +0200

    mising plugins entires of previous 2 commits

--- src/config/nvim/lua/plugins.lua
+++ src/config/nvim/lua/plugins.lua
@@ -33,18 +33,29 @@ return require('packer').startup(function(use)
     }
 
     use 'tpope/vim-fugitive'
-end)
 
---
---    use {
---        "neovim/nvim-lspconfig",
---        opt = true,
---        event = "BufRead",
---        wants = { "nvim-lsp-installer" },
---        config = function()
---            require("config.lsp").setup()
---        end,
---        requires = {
---            "williamboman/nvim-lsp-installer",
---        },
---    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+
+    use 'mbbill/undotree'
+
+end)
