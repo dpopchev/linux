81c083f135c5e79c148cb9377717156e3c05edc4
dpopchev <dpopchev@gmail.com>
2023-01-04 15:15:10 +0200

    refactor(scratch.lua): mappings and configs

--- src/config/nvim/after/plugin/scratch.lua
+++ src/config/nvim/after/plugin/scratch.lua
@@ -1,5 +1,3 @@
-vim.g.scratch_autohide = "&hidden"
-vim.g.scratch_insert_autohide = 0
 vim.g.scratch_filetype = 'markdown'
 vim.g.scratch_horizontal = 0
 vim.g.scratch_top = 0
@@ -7,8 +5,6 @@ vim.g.scratch_height = 60
 vim.g.scratch_persistence_file = '.scratch.vim'
 
 vim.g.scratch_no_mappings = 1
-vim.cmd [[ nnoremap <leader>gss :ScratchPreview<cr> ]]
-vim.cmd [[ nmap <leader>gsi <plug>(scratch-insert-reuse) ]]
-vim.cmd [[ nmap <leader>gsC <plug>(scratch-insert-clear) ]]
-vim.cmd [[ xmap <leader>gsi <plug>(scratch-selection-reuse) ]]
-vim.cmd [[ xmap <leader>gsI <plug>(scratch-selection-clear) ]]
+vim.cmd [[ nnoremap <leader>si :ScratchInsert<cr> ]]
+vim.cmd [[ vnoremap <leader>si :ScratchSelection<cr> ]]
+vim.cmd [[ nnoremap <leader>ss :ScratchPreview<cr> ]]
