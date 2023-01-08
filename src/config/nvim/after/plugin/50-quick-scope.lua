9d3b93ed5ade6cb4cfef0512a12cb7948a8b97c8
dpopchev <dpopchev@gmail.com>
2023-01-08 13:00:21 +0200

    feat(quick-scope): add higlight costumization lines; commands from offical git

--- src/config/nvim/after/plugin/50-quick-scope.lua
+++ src/config/nvim/after/plugin/50-quick-scope.lua
@@ -1,2 +1,10 @@
 vim.g.qs_max_chars = 120
 vim.g.qs_buftype_blacklist = {'terminal', 'nofile'}
+
+vim.cmd [[
+augroup qs_colors
+  autocmd!
+  autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+  autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+augroup END
+]]
