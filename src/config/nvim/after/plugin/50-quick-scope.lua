de12ed69ffe944a11e295bbb5b0fd98d36643426
dpopchev <dpopchev@gmail.com>
2023-01-08 13:10:34 +0200

    rm quickscope hi commands

--- src/config/nvim/after/plugin/50-quick-scope.lua
+++ src/config/nvim/after/plugin/50-quick-scope.lua
@@ -1,10 +1,2 @@
 vim.g.qs_max_chars = 120
 vim.g.qs_buftype_blacklist = {'terminal', 'nofile'}
-
-vim.cmd [[
-augroup qs_colors
-  autocmd!
-  autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
-  autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
-augroup END
-]]
