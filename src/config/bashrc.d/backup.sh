9935f356666932c4c0b4a5ff65305c61e52bf265
dpopchev <dpopchev@gmail.com>
2023-01-17 01:01:20 +0200

    feat(backup): test arrya gathering of flags and passing to function

--- src/config/bashrc.d/backup.sh
+++ src/config/bashrc.d/backup.sh
@@ -13,3 +13,13 @@ log () {
     local timestamp=$(date "+%d-%m-%Y %H:%M:%S")
     printf "${timestamp} -- ${*}\n" >> ${LOGFILE}
 }
+
+backup () {
+    local day=$(date +%U-%A)
+    local arguments=()
+    # arguments+=(-a --delete --quite --inplace)
+    # arguments+=(--backup --backup-dir=${DST}/${day})
+    # arguments+=(${SRC})
+    arguments+=('-T' '5')
+    wget ${arguments[*]} 192.168.1.99:5001
+}
