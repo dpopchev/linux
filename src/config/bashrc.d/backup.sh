89a528fde2934e9a91c605a76093d8867ef74dfb
dpopchev <dpopchev@gmail.com>
2023-01-17 07:36:08 +0200

    feat(bakcu): add rsync cmd

--- src/config/bashrc.d/backup.sh
+++ src/config/bashrc.d/backup.sh
@@ -6,6 +6,7 @@ help () {
     echo "expecting external variables"
     echo "-- BACKUP_USER, remote host user"
     echo "-- BACKUP_HOST, remote host"
+    echo "-- BACKUP_DIR, remote host"
     echo "-- BACKUP_PASS, passphrase to be used"
     echo "-- BACKUP_TARGETS, path-like variable listing backup targets"
     echo "Find log at: ${LOGFILE}"
@@ -19,14 +20,18 @@ log () {
     printf "${timestamp} -- ${*}\n" >> ${LOGFILE}
 }
 
-backup () {
-    log 'INFO -- Starting backup'
+backup_dir () {
+    log "INFO -- Starting backup_dir for ${1}"
     local day=$(date +%U-%A)
     local options=('-a' '--delete' '--quiet')
-    wget ${arguments[*]} ${BACKUP_HOST}:5001 >> ${LOGFILE} 2>&1
+
+    rsync "${arguments[*]}" "${1}" "${BACKUP_USER}@${BACKUP_HOST}:${BACKUP_DIR}" >> ${LOGFILE} 2>&1
+
     if [[ $? -eq 0 ]]; then
         log 'INFO -- backup succeed'
     else
         log 'ERROR -- backup failed'
     fi
 }
+
+backup_dir
