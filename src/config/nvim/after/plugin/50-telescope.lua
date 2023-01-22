b5b4403dcc81f9e66aa22455ff1bd60a35b0163e
dpopchev <dpopchev@gmail.com>
2023-01-22 11:58:19 +0200

    refactor(telescope): serch mapping

--- src/config/nvim/after/plugin/50-telescope.lua
+++ src/config/nvim/after/plugin/50-telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ef', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>eS', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>es', function()
+vim.keymap.set('n', '<leader>eG', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>eg', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
