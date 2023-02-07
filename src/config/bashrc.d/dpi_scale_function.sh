4e36d2e1bd81bfe358d8bae566f2959eaae69da7
dimitar popchev <dpopchev@gmail.com>
2023-02-07 15:04:31 +0200

    fix typos

--- src/config/bashrc.d/dpi_scale_function.sh
+++ src/config/bashrc.d/dpi_scale_function.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 function scale_dpi {
-    local dpi=$(xdpyinfo | grep dots | grep -P -o '\b\d\d')
-    xrdb -merge <(printf "Xft.dpi: %0.f" $(bc -l <<< "${dpi}*$1"))
+    local dpi=$(xdpyinfo | grep dots | grep -P -o '\b\d+')
+    xrdb -merge <(printf "Xft.dpi: %.0f" $(bc -l <<< "${dpi}*$1"))
 }
