ab8504a800cb5ed06df3342e7cd967bb26032e41
Dimitar Popchev <dpopchev@gmail.com>
2023-02-11 10:52:40 +0200

    refactor separtte operations

--- src/config/bashrc.d/dpi_scale_function.sh
+++ src/config/bashrc.d/dpi_scale_function.sh
@@ -2,5 +2,6 @@
 
 function scale_dpi {
     local dpi=$(xdpyinfo | grep dots | grep -P -o '\b\d+')
-    xrdb -merge <(printf "Xft.dpi: %.0f" $(bc -l <<< "${dpi}*$1"))
+    local new_dpi=$(printf "%.0f" $(bc -l <<< "${dpi}*$1"))
+    echo "Xft.dpi: ${new_dpi}" | xrdb -merge
 }
