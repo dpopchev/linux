6a76fea4695f7e24339c4a09b513cac8e7fdfd0f
Dimitar Popchev <dpopchev@gmail.com>
2023-04-06 23:10:48 +0300

    feat xidlehook

---
+++ src/dpopchev/start_xidlehook.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Run xidlehook
+xidlehook \
+  `# Don't lock when there's a fullscreen application` \
+  --not-when-fullscreen \
+  `# Don't lock when there's audio playing` \
+  --not-when-audio \
+  `# Dim the screen after 60 seconds, undim if user becomes active` \
+  --timer 420 \
+    'brightnessctl -s; brightnessctl s 10%-' \
+    'brightnessctl -r' \
+  `# Undim & lock after 10 more seconds` \
+  --timer 10 \
+    'brightnessctl -r; bash ~/.dpopchev/lock.sh' \
+    '' \
+  `# Finally, suspend an hour after it locks` \
+  --timer 900 \
+    'systemctl suspend' \
+    ''
