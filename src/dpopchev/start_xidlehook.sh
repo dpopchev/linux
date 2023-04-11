fb8aa5969a62b71eb04aea0622189d0ba4e40079
Dimitar Popchev <dpopchev@gmail.com>
2023-04-11 17:45:41 +0300

    chore: duty message for small self-explanatory changes

--- src/dpopchev/start_xidlehook.sh
+++ src/dpopchev/start_xidlehook.sh
@@ -2,19 +2,20 @@
 
 # Run xidlehook
 xidlehook \
-  `# Don't lock when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 60 seconds, undim if user becomes active` \
-  --timer 420 \
+    --detect-sleep \
+    `# Don't lock when there's a fullscreen application` \
+    --not-when-fullscreen \
+    `# Don't lock when there's audio playing` \
+    --not-when-audio \
+    `# Dim the screen after 60 seconds, undim if user becomes active` \
+    --timer 420 \
     'brightnessctl -s; brightnessctl s 10%-' \
     'brightnessctl -r' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 10 \
+    `# Undim & lock after 10 more seconds` \
+    --timer 10 \
     'brightnessctl -r; bash ~/.dpopchev/lock.sh' \
     '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 20 \
+    `# Finally, suspend an hour after it locks` \
+    --timer 20 \
     'systemctl suspend' \
     ''
