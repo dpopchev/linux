499853461b18257b691772961041917f721135f8
Dimitar Popchev <dpopchev@gmail.com>
2023-05-09 22:50:12 +0300

    fix no brightness change before lock

--- src/dpopchev/start_xidlehook.sh
+++ src/dpopchev/start_xidlehook.sh
@@ -6,8 +6,8 @@ xidlehook \
     --not-when-fullscreen \
     --not-when-audio \
     --timer 420 \
-    'brightnessctl -s; brightnessctl s 10%-' \
-    'brightnessctl -r' \
+    '' \
+    '' \
     --timer 10 \
     'brightnessctl -r; bash ~/.dpopchev/lock.sh' \
     '' \
