9299e5d88096ab85d81efed88c2c37cf5563a840
Dimitar Popchev <dpopchev@gmail.com>
2024-06-25 09:37:39 +0300

    refactor: leave explicit mini indentscope

--- roles/nvim/files/plugins/mini_nvim.lua
+++ roles/nvim/files/plugins/mini_nvim.lua
@@ -1,13 +1,5 @@
-return { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-        -- Better Around/Inside textobjects
-        --
-        -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [']quote
-        --  - ci'  - [C]hange [I]nside [']quote
-        require('mini.ai').setup { n_lines = 500 }
-        require('mini.indentscope').setup()
-    end,
+return {
+    'echasnovski/mini.indentscope',
+    version = false,
+    opts = {},
 }
