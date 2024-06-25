4447862b674b924459ec4017b1298a3d8831c11d
Dimitar Popchev <dpopchev@gmail.com>
2024-06-25 11:27:37 +0300

    refactor:

--- roles/nvim/files/plugins/git_conflict.lua
+++ roles/nvim/files/plugins/git_conflict.lua
@@ -1,7 +1,7 @@
 return {
     'akinsho/git-conflict.nvim',
     version = "*",
-    config = {
+    opts = {
         disable_diagnostics = true,
         highlights = { -- They must have background color, otherwise the default color will be used
             incoming = 'DiffAdd',
