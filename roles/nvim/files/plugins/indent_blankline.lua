b69e760b2a65616ab63c3ca89da82132dd66b963
Dimitar Popchev <dpopchev@gmail.com>
2024-09-02 21:29:41 +0300

    feat: indent blankline disable at first level

--- roles/nvim/files/plugins/indent_blankline.lua
+++ roles/nvim/files/plugins/indent_blankline.lua
@@ -5,6 +5,9 @@ local function config_factory()
             show_end = false
         }
     })
+    local hooks = require("ibl.hooks")
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
 end
 
 return {
