fba6f82df8650c042ab235203b484add76275df4
Dimitar Popchev <dpopchev@gmail.com>
2024-09-11 17:19:19 +0300

    feat: migrate to nvim tree

---
+++ roles/nvim/files/plugins/nvim_tree.lua
@@ -0,0 +1,43 @@
+local function opts(desc)
+    return {
+        desc = "nvim-tree: " .. desc,
+        noremap = true,
+        silent = false,
+    }
+end
+
+local function config_factory()
+    local function on_attach(bufnr)
+        local api = require "nvim-tree.api"
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- custom mappings
+        -- vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+    end
+
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    local conf = { on_attach = on_attach }
+
+    local api = require "nvim-tree.api"
+    vim.keymap.set('n', '<leader>et', function() api.tree.toggle({ find_file = false, focus = true }) end,
+        opts('[t]oggle'))
+    vim.keymap.set('n', '<leader>eT', function() api.tree.toggle({ find_file = true, focus = true }) end,
+        opts('[T]raverse cursor to current buffer'))
+    vim.keymap.set('n', '<leader>ec', function() api.tree.collapse_all(true) end,
+        opts('[C]ollapse recursively tree keeping open buffers'))
+    vim.keymap.set('n', '<leader>eC', function() api.tree.collapse_all(false) end,
+        opts('[C]ollapse all recursively'))
+
+    require('nvim-tree').setup(conf)
+end
+
+
+return {
+    'nvim-tree/nvim-tree.lua',
+    version = "*",
+    lazy = false,
+    config = config_factory,
+}
